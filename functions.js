840eca8271fa70db8b8d0f048354213c600e4025
Kyle So <kyle.p.so@gmail.com>
2019-01-16 10:00:54 -0800

    added functions.js and objects.js

---
+++ functions.js
@@ -0,0 +1,18 @@
+// Functions
+
+// Exercise: Rude Bot
+
+function insult(name) {
+    // Variables created inside the block function will cease
+    // exist outside of the block. They'll disappear after
+    // the function has finished executing.
+    const randNum = Math.floor(Math.random() * 3);
+  
+    if (randNum === 0) {
+      return `${name}, you doofus`;
+    } else if (randNum === 1) {
+      return `${name}, your father smelt of elderberries`;
+    } else {
+      return `Your mother was a hamster, ${name}`;
+    }
+    // When you `return` from a function, the remaining lines
---
+++ objects.js
@@ -0,0 +1,18 @@
+// Objects
+
+// Creating an object
+
+const keyName = "title";
+
+// prettier-ignore
+const daeny = {
+  // ð a key
+  firstName: "Daenerys",
+  //            ð a value
+  "lastName": "Targaryen",
+  // Keys are always string even if they do not look like it
+  // Values can be any type (e.g. number, string, object, function, etc)
+  [keyName + "s"]: [
+  // To get the key from expression, surround the expression
+  // with []
+    "Queen of Mereen",
