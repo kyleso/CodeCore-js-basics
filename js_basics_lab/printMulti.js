527e6ba572ad2c98599596bc213fbe224d0b6518
Kyle So <kyle.p.so@gmail.com>
2019-01-16 15:27:34 -0800

    modifed printMulti.js

--- js_basics_lab/printMulti.js
+++ js_basics_lab/printMulti.js
@@ -8,3 +8,13 @@ function printMulti(arr) {
 
 const myArray = [[2,3,4], ["Hello CodeCore", 1, true]];
 printMulti(myArray);
+
+/*
+
+function printMulti(array) {
+    for (let arr of array) {
+        for (let val of arr) {
+            console.log(val);
+        }
+    }
+}
