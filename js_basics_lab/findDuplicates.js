ccf8f9407d719eb9a4c7b52f7baec255f4349553
Kyle So <kyle.p.so@gmail.com>
2019-01-18 18:17:53 -0800

    correct findDuplicates.js

--- js_basics_lab/findDuplicates.js
+++ js_basics_lab/findDuplicates.js
@@ -1,31 +1,49 @@
 /*
+
 function findDuplicates(values) {
     
     console.log(values);
     let obj = {};
     let result = [];
-    let finalArray = [];
-    let iterationCounter = 0;
 
     for (let i = 0; i < values.length; i += 1) {
-        obj[values[i]] = 0;
-        iterationCounter += 1;
-        //console.log(iterationCounter);
-        console.log(obj);
-        if (obj.length !== result.length) {
-            let x = (result[iterationCounter]);
-            console.log(x);
-            finalArray.push(x);
-        }   
+        
+        if (!obj[values[i]] && obj[values[i]] !== 0 ) {
+            obj[values[i]] = 0;
+        } else {
+            obj[values[i]] += 1;
+        }
+            // let x = (result[iterationCounter]);
+            // console.log(x);
+            // finalArray.push(x); 
     } 
     for (let i in obj) {
-        result.push(i);
-        console.log(result);
+        if(obj[i]) {
+            result.push(i);
+        }
     }
-    return finalArray;
+
+    return result;
 }
 
 console.log(findDuplicates([1, 1, 3, 4, 5, 6, 7, 8, 9, 9])); // returns [1, 9]
 console.log(findDuplicates(['Bob', 'bob', 'Rob', 'Rob', 1, 2, 3, 4, 4])); // returns ['Rob', 4]
 
 */
+
+function findDuplicates(values) {
+
+return [...new Set(values.filter(function(a){
+    return values.indexOf(a) !== values.lastIndexOf(a)
+}))];
+}
+
+function myFunc(arr) {
+    return arr.filter(function (value, index, arr) {
+        return arr.indexOf(value) !== index;
+    });
+}
+
+console.log(findDuplicates([1, 1, 3, 4, 5, 6, 7, 8, 9, 9])); // returns [1, 9]
+console.log(findDuplicates(['Bob', 'bob', 'Rob', 'Rob', 1, 2, 3, 4, 4])); // returns ['Rob', 4]
+
